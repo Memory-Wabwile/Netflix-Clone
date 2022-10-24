47dc23b204e2cf031e84bfd47a801902dcfe6bd7
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-24 09:18:40 +0300

    create a row file for displaying the movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import requests from './request';
+import Row from './Row';
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+
+     < Row title=" NETFLIX ORIGINALS " fetchUrl={requests.fetchNetflixOriginals} />
+     < Row title=" Trending Now " fetchUrl={requests.fetchTrending} />
+
     </div>
   );
 }
