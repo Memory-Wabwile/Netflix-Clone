7d1a78738b0193746763d81c45a0d5b4b6fd8932
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-24 10:50:38 +0300

    create the row.css file and style the movie posters

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import requests from './request';
 import Row from './Row';
-import requests from './request';
+
 
 function App() {
   return (
--- src/Row.js
+++ src/Row.js
@@ -1,24 +1,36 @@
-import React , {useState , useEffect} from 'react'
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import './Row.css'
 
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title}) {
-  const[movies,setMovies] = useState([]);
-    
-    // code which runs based on specific conditions
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-  useEffect(()=>{
+  // code which runs based on specific conditions
+
+  useEffect(() => {
     // if [] , run once when the row loads and dont run again
-    async function fetchData(){
+    async function fetchData() {
       const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-  } , []); 
+  }, [fetchUrl]);
+
   return (
-    <div>
-        <h2>{title}</h2>
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
