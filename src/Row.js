204898fc30df02dd8f32ccac1f7fab62cd7a4155
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-24 09:36:09 +0300

    write an async function to fetch data

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,19 @@
-import React , {useState} from 'react'
+import React , {useState , useEffect} from 'react'
+import axios from './axios';
+
 
 function Row({title}) {
-    const[movies,setMovies] = useState([]);
+  const[movies,setMovies] = useState([]);
     
+    // code which runs based on specific conditions
+
+  useEffect(()=>{
+    // if [] , run once when the row loads and dont run again
+    async function fetchData(){
+      const request = await axios.get(fetchUrl);
+    }
+    fetchData();
+  } , []); 
   return (
     <div>
         <h2>{title}</h2>
