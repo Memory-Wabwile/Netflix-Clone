0232758765d7778782f21bff7bf7292d48fe59ab
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-26 13:00:52 +0300

    add animations

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
 import "./Navbar.css";
 
 function Navbar() {
+    const[show , handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll" , () => {
+            if(window.scrollY > 100){
+                handleShow(true);
+            }else handleShow(false);
+        });
+        return() => {
+            window.removeEventListener("scrollY");
+        };
+    } , []);
+
+
   return (
-    <div className='Nav' >
+    <div className={`Nav ${show && "nav_black"}`} >
         <img className='Nav_logo' 
         src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
         alt='Netflix Logo' />
