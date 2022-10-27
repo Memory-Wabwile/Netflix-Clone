edc9d42c8f1ec585f64451ff8f1e7136fb2bbd56
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-27 12:23:11 +0300

    style the footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,71 @@ import "./Footer.css";
 function Footer() {
   return (
     <div>
-        
+        <footer className='footer' >
+            <p>Question ? Call 0117-890-234</p>
+
+            <div className='footer-cols' >
+                <ul>
+                    <li>
+                        <a href='#'> FAQ </a>
+                    </li>
+                    <li>
+                        <a href='#'> Investor Relations</a>
+                    </li>
+                    <li>
+                        <a href='#'>Corporate Information</a>
+                    </li>
+                    <li>
+                        <a href='#'>Netflix Originals</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href='#'> Help Center </a>
+                    </li>
+                    <li>
+                        <a href='#'> Jobs</a>
+                    </li>
+                    <li>
+                        <a href='#'>Terms of Use</a>
+                    </li>
+                    <li>
+                        <a href='#'>Contact Us</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href='#'> Account </a>
+                    </li>
+                    <li>
+                        <a href='#'> Redeem Gift Cards</a>
+                    </li>
+                    <li>
+                        <a href='#'>Privacy</a>
+                    </li>
+                    <li>
+                        <a href='#'>Speed Test</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href='#'> Media Center </a>
+                    </li>
+                    <li>
+                        <a href='#'> Buy Gift Cards</a>
+                    </li>
+                    <li>
+                        <a href='#'>Cookie Preferences</a>
+                    </li>
+                    <li>
+                        <a href='#'>Legal Notices</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
     </div>
   )
 }
