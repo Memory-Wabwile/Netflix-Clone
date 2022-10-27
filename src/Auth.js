48404c1cd13ef98ce6d46ef6f26346d2c328d50b
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-27 12:11:47 +0300

    create the footer component

--- src/Auth.js
+++ src/Auth.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import "./Auth.css";
 
 const Auth = () => {
   return (
     <header className='showcase' >
         <div className='showcase-top' >
-            <img src='https://i.bb.co/r5krrdz/logo.png' alt='logo'/>
-            <Link to="/" className="btn btn rounded" >
+          
+            <img src='https://o.remove.bg/downloads/8ed35ce7-8985-4ab2-89c9-f26b2acc5ded/Netflix-logo-red-black-png-removebg-preview.png' alt='logo'/>
+            <Link to="/" className="btn btn-rounded" >
               Sign In
             </Link>
 
@@ -14,7 +16,7 @@ const Auth = () => {
                 <h1>Unlimited movies , TV shows and more</h1>
                 <p> Watch anywhere , Cancel anytime</p>
 
-                <Link to="/home"> Watch free for 30 days</Link>
+                <Link to="/home" className='btn btn-xl' > Watch free for 30 days </Link>
             </div>
         </div>
     </header>
