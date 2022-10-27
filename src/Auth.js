0c048df005ca312f38c28a36021098b3b919bbda
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-27 10:20:29 +0300

    create a home file and transfer files to it

--- src/Auth.js
+++ src/Auth.js
@@ -6,7 +6,16 @@ const Auth = () => {
     <header className='showcase' >
         <div className='showcase-top' >
             <img src='https://i.bb.co/r5krrdz/logo.png' alt='logo'/>
+            <Link to="/" className="btn btn rounded" >
+              Sign In
+            </Link>
 
+            <div className='showcase-content' >
+                <h1>Unlimited movies , TV shows and more</h1>
+                <p> Watch anywhere , Cancel anytime</p>
+
+                <Link to="/App"> Watch free for 30 days</Link>
+            </div>
         </div>
     </header>
   )
