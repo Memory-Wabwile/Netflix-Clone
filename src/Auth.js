b2f16dad100abd16d06be75515132354fe7a7cf7
Memory-Wabwile <memorywabwile@gmail.com>
2022-10-27 09:53:05 +0300

    create a showcase div for the top image and details

--- src/Auth.js
+++ src/Auth.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Auth = () => {
   return (
-    <header>
-      
+    <header className='showcase' >
+        <div className='showcase-top' >
+            <img src='https://i.bb.co/r5krrdz/logo.png' alt='logo'/>
+
+        </div>
     </header>
   )
 }
